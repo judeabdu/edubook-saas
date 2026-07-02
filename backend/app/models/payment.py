c533af35dee9490e2916094abec3912d3c61d808
judeabdu <abdushakuluhssenkindu@gmail.com>
2026-07-02 12:09:32 +0000

    initial push

--- backend/app/models/payment.py
+++ backend/app/models/payment.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import uuid
 
 from app.core.database import Base
-from app.models.school import School  # Import the existing School model
 
 class MobileMoneyTransaction(Base):
     __tablename__ = "momo_transactions"
