e43e164e4614cb0159297c3c1db4320ff7405d9e
judeabdu <abdushakuluhssenkindu@gmail.com>
2026-07-08 07:37:13 +0000

    fix: remove duplicated navbar from home route layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Stats from '../components/Stats';
 import Features from '../components/Features';
@@ -11,7 +10,6 @@ import Footer from '../components/Footer';
 export default function Home() {
   return (
     <div className="bg-white min-h-screen text-gray-900 antialiased">
-      <Navbar />
       <main>
         <Hero />
         <Stats />
